d1bdbdaaf5843f7a423615215fa5fb11088147f2
Omega88888 <lahoathanh35@gmail.com>
2026-07-06 03:03:06 +0700

    Fix Pint style

---
+++ Modules/Attendance/App/Transformers/AttendanceCollection.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Modules\Attendance\App\Transformers;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class AttendanceCollection extends ResourceCollection
+{
+    public function toArray($request)// cái này cho phép xuất ra data table
+    {
+        return [
+
+            'data' => $this->collection,
+
+            'links' => [
+                'first' => $this->resource->url(1),
+                'last' => $this->resource->url($this->resource->lastPage()),
+            ],
+
+            'meta' => [
+                'current_page' => $this->resource->currentPage(),
+                'last_page' => $this->resource->lastPage(),
+                'total' => $this->resource->total(),
+            ],
+        ];
+    }
+}
+// file này đang dư
